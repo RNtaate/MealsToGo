c6c1252981b5c90669906925e1768a7c20d31360
RNtaate <royhyde14@gmail.com>
2022-10-25 02:22:37 +0300

    Import specific font families from google fonts
    Import their sizes as well, and the useFonts hook

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts as oswaldUseFont, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { useFonts as latoUseFont, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import RestaurantsScreen from './src/features/restaurants/screens/RestaurantsScreen';
 import { theme } from './src/features/restaurants/infrastructure/theme'
 
 const isAndroid = Platform.OS
 export default function App() {
+
+  const [oswaldFontFamily] = oswaldUseFont({ Oswald_400Regular });
+  const [latoFontFamily] = latoUseFont({ Lato_400Regular })
+
+  if(!oswaldFontFamily || !latoFontFamily) {
+    return null
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
