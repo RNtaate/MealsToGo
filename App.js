0b058b0a499dd3f5f91f5503d8817ddc71c468ed
RNtaate <royhyde14@gmail.com>
2022-10-24 21:33:20 +0300

    Add and design a mock up of the application screen

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 const isAndroid = Platform.OS
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+        <Searchbar placeholder='Search'/>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
       <ExpoStatusBar />
-      <Text>Meals To Go App</Text>
-    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#353',
+    backgroundColor: 'white',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  searchContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  listContainer: {
+    backgroundColor: '#123',
+    flex: 1
   }
 })
