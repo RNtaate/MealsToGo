12c4f42bf5d7a2f4b1a45a04f6952befad366f25
RNtaate <royhyde14@gmail.com>
2022-11-10 03:36:05 +0300

    Mock fetching of data using the Restaurant context and supplying the data to the application

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,11 +1,41 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { restaurantsRequest, restaurantTransform } from './restaurants.service';
 
 export const RestaurantContext = createContext()
 
 const RestaurantContextProvider = ({ children }) => {
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [ error, setError] = useState("");
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+      .then(response => {
+        return restaurantTransform(response)
+      })
+      .then(transformedResults => {
+        setIsLoading(false);
+        setRestaurants(transformedResults)
+      })
+      .catch( err => {
+        setIsLoading(false);
+        setError(err)
+      })
+    }, 5000)
+  }
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, [])
+
   return (
     <RestaurantContext.Provider value={{
-      restaurants: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      restaurants,
+      isLoading,
+      error
     }}>
       {children}
     </RestaurantContext.Provider>
