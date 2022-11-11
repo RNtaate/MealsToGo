d352f46180889e02b13355e0293707655f147a99
RNtaate <royhyde14@gmail.com>
2022-11-11 14:00:18 +0300

    Create location.context.js, add location context to it.

---
+++ src/services/location/location.context.js
@@ -0,0 +1,50 @@
+import React, { createContext, useState, useEffect } from "react";
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+const LocationContextProvider = ({ children }) => {
+
+  const [ location, setLocation ] = useState(null);
+  const [ keyWord, setKeyWord ] = useState("San Francisco");
+  const [ isLocationLoading, setIsLocationLoading] = useState(false);
+  const [ locationError, setLocationError ] = useState(null);
+
+  const locationSearch = (searchKeyWord = "Antwerp") => {
+    setIsLocationLoading(true);
+    setKeyWord(searchKeyWord)
+    locationRequest(searchKeyWord.toLowerCase())
+    .then((response) => {
+      return locationTransform(response);
+    })
+    .then(transformedResult => {
+      setIsLocationLoading(false);
+      setLocation(transformedResult);
+      console.log(transformedResult);
+    })
+    .catch( err => {
+      setIsLocationLoading(false);
+      setLocationError(err);
+      console.log(err);
+    })
+
+  }
+
+  useEffect(() => {
+    locationSearch();
+  }, [])
+
+  return (
+    <LocationContext.Provider value={{
+      location,
+      keyWord,
+      isLocationLoading,
+      locationError,
+      locationSearch
+    }}>
+      { children }
+    </LocationContext.Provider>
+  )
+}
+
+export default LocationContextProvider;
