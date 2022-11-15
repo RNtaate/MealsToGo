f3f53b8c7f64d170c7511b1c6388095e4cac7aef
RNtaate <royhyde14@gmail.com>
2022-11-16 02:45:52 +0300

    Remove unwanted code resulting from creating seperate RestaurantDetailsScreen

--- src/features/restaurants/infrastructure/navigation/restaurants.navigator.js
+++ src/features/restaurants/infrastructure/navigation/restaurants.navigator.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 
 import RestaurantsScreen from '../../screens/RestaurantsScreen';
-import { Text } from 'react-native';
+import { RestaurantDetailsScreen } from '../../screens/RestaurantDetailsScreen';
 
 const RestaurantStack = createStackNavigator();
-
-const RestaurantDetails = () => {
-  return (
-    <Text>Something</Text>
-  )
-}
 
 export const RestaurantsNavigation = () => {
   return (
     <RestaurantStack.Navigator screenOptions={{...TransitionPresets.ModalPresentationIOS, headerShown: false}}>
       <RestaurantStack.Screen name='RestaurantsScreen' component={RestaurantsScreen}/>
-      <RestaurantStack.Screen name='RestaurantsDetails' component={RestaurantDetails}/>
+      <RestaurantStack.Screen name='RestaurantsDetails' component={RestaurantDetailsScreen}/>
     </RestaurantStack.Navigator>
   )
 }
