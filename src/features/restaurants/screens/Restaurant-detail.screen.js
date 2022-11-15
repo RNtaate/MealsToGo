2ae0c85c8d074a31b0b622b4d92eb3b682c1449e
RNtaate <royhyde14@gmail.com>
2022-11-16 02:58:37 +0300

    Update styling for DetailsView

--- src/features/restaurants/screens/Restaurant-detail.screen.js
+++ src/features/restaurants/screens/Restaurant-detail.screen.js
@@ -3,10 +3,11 @@ import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
 
 import RestaurantInfoCard from '../components/Restaurant-info-card.component';
+import { SafeArea } from '../../../components/utility/safe-area.component';
 
 const DetailsView = styled(View)`
   flex: 1;
-  padding: ${({theme}) =>`${theme.space[2]} ${theme.space[3]}`};
+  padding: ${({theme}) => theme.space[2]};
 `;
 
 export const RestaurantDetailsScreen = ({ route }) => {
