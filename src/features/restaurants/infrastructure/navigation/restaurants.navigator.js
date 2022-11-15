de9f5cb84179db86a9979e78468496a7bc4419f4
RNtaate <royhyde14@gmail.com>
2022-11-16 02:19:58 +0300

    Add a second screen to the restaurants navigation stack
    Make the second screen accessible using TouchableOpacity and the navigation prop

--- src/features/restaurants/infrastructure/navigation/restaurants.navigator.js
+++ src/features/restaurants/infrastructure/navigation/restaurants.navigator.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 
 import RestaurantsScreen from '../../screens/RestaurantsScreen';
+import { Text } from 'react-native';
 
 const RestaurantStack = createStackNavigator();
 
+const RestaurantDetails = () => {
+  return (
+    <Text>Something</Text>
+  )
+}
+
 export const RestaurantsNavigation = () => {
   return (
-    <RestaurantStack.Navigator screenOptions={{headerShown: false}}>
+    <RestaurantStack.Navigator screenOptions={{...TransitionPresets.ModalPresentationIOS, headerShown: false}}>
       <RestaurantStack.Screen name='RestaurantsScreen' component={RestaurantsScreen}/>
+      <RestaurantStack.Screen name='RestaurantsDetails' component={RestaurantDetails}/>
     </RestaurantStack.Navigator>
   )
 }
