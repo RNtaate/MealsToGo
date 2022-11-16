ce8b83708281cd3cda97e7416455f6c1dfeb89ad
RNtaate <royhyde14@gmail.com>
2022-11-16 15:32:22 +0300

    Remove unwanted import

--- src/features/restaurants/screens/Restaurant-detail.screen.js
+++ src/features/restaurants/screens/Restaurant-detail.screen.js
@@ -4,7 +4,6 @@ import styled from 'styled-components/native';
 import { List } from 'react-native-paper';
 
 import RestaurantInfoCard from '../components/Restaurant-info-card.component';
-import { theme } from '../infrastructure/theme';
 
 const DetailsView = styled(View)`
   flex: 1;
