8403cacbbe5a9b14f2669e0d448d8d6f39181561
RNtaate <royhyde14@gmail.com>
2022-11-24 02:28:38 +0300

    Modify the design of the FavouritesBar using CompactRestaurant info card using

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { View, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
 
 import { Text } from '../typography/text.component';
+import { CompactRestaurantInfo } from '../restaurant/compact-restaurant-info.component';
+import { Spacer } from '../Spacer/Spacer.component';
 
-const FavouritesBar = () => {
+const FavouritesWrapper = styled(View)`
+  padding: ${({theme}) => `${theme.space[2]} ${theme.space[3]}`};
+`;
+
+const FavouritesBar = ({ favourites }) => {
+  if(!favourites.length) {
+    return null;
+  }
+
   return(
-    <Text variant="caption" >Favourites</Text>
+    <FavouritesWrapper>
+      <Spacer position={"bottom"}>
+        <Text variant="caption" >Favourites</Text>
+      </Spacer>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} >
+        {favourites.map((restaurant) => {
+          return(
+            <Spacer position={"horizontal"} size={"small"} >
+              <CompactRestaurantInfo restaurant={restaurant}/>
+            </Spacer>
+          )
+        })}
+      </ScrollView>
+    </FavouritesWrapper>
   )
 }
 
