7d88379c39d5cb1184789a51d891ad6e2dabbbde
RNtaate <royhyde14@gmail.com>
2022-11-24 01:40:43 +0300

    Create a mock design for a favourites bar

---
+++ src/components/favourites/favourites-bar.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text } from '../typography/text.component';
+
+const FavouritesBar = () => {
+  return(
+    <Text variant="caption" >Favourites</Text>
+  )
+}
+
+export default FavouritesBar;
