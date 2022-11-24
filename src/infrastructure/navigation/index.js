f2dd68f22844863072885c70da541567aebb3b2c
RNtaate <royhyde14@gmail.com>
2022-11-25 01:36:43 +0300

    Add AccountNavigator to the Navigation component
    Add mock implementation of authentication check

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
 
 import AppNavigator from './app.navigator';
+import AccountNavigator from './account.navigator';
+import { AuthenticationContext } from '../../services/authentication/authentication.context';
 
 const Navigation = () => {
+
+  const { user } = useContext(AuthenticationContext);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <AppNavigator />
+    <NavigationContainer>
+      { isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
   )
 }
 
