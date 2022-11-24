b91553057cf964b4e4fd1ed874bd1d83ad3a5588
RNtaate <royhyde14@gmail.com>
2022-11-24 03:00:11 +0300

    Add keys to the Favourites Compact Restaurant info cards
    Update styling for the Favourites View

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -8,6 +8,7 @@ import { Spacer } from '../Spacer/Spacer.component';
 
 const FavouritesWrapper = styled(View)`
   padding: ${({theme}) => `${theme.space[2]} ${theme.space[3]}`};
+  margin-bottom: ${({theme}) => theme.space[2]}
 `;
 
 const FavouritesBar = ({ favourites, onNavigate }) => {
@@ -22,8 +23,9 @@ const FavouritesBar = ({ favourites, onNavigate }) => {
       </Spacer>
       <ScrollView horizontal showsHorizontalScrollIndicator={false} fadingEdgeLength={20} >
         {favourites.map((restaurant) => {
+          const key = restaurant.name
           return(
-            <Spacer position={"horizontal"} size={"small"} >
+            <Spacer position={"horizontal"} size={"small"} key={key} >
               <TouchableOpacity onPress={() => onNavigate("RestaurantsDetails", {restaurant})}>
                 <CompactRestaurantInfo restaurant={restaurant}/>
               </TouchableOpacity>
