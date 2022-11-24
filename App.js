d204cd3aae205bfb5dd83f8a2875aa5152454aa4
RNtaate <royhyde14@gmail.com>
2022-11-25 01:10:33 +0300

    Transfer the firebase configuration and initialization code out of the App.js file
    Place the code in authentication services

--- App.js
+++ App.js
@@ -3,30 +3,15 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts as oswaldUseFont, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as latoUseFont, Lato_400Regular } from '@expo-google-fonts/lato';
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 import { theme } from './src/infrastructure/theme';
 import RestaurantContextProvider from './src/services/restaurants/restaurants.context';
 import LocationContextProvider from './src/services/location/location.context';
 import FavouritesContextProvider from './src/services/favourites/favourites.context';
 import AuthenticationContextProvider from './src/services/authentication/authentication.context';
-import AppNavigator from './src/infrastructure/navigation/app.navigator';
+import Navigation from './src/infrastructure/navigation';
 
 const isAndroid = Platform.OS
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAYPkx_zOZmfNvQloA9ZkjZh80xtI56_Ns",
-  authDomain: "mealstogo-7bc9a.firebaseapp.com",
-  projectId: "mealstogo-7bc9a",
-  storageBucket: "mealstogo-7bc9a.appspot.com",
-  messagingSenderId: "1007401999647",
-  appId: "1:1007401999647:web:3a45fe5a0f58724969242a"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
 export default function App() {
 
   const [oswaldFontFamily] = oswaldUseFont({ Oswald_400Regular });
@@ -43,7 +28,7 @@ export default function App() {
           <FavouritesContextProvider>
             <LocationContextProvider>
               <RestaurantContextProvider>
-                <AppNavigator />
+                <Navigation />
               </RestaurantContextProvider>
             </LocationContextProvider>
           </FavouritesContextProvider>
--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,5 +1,17 @@
-import { auth } from "../../../App";
-import { signInWithEmailAndPassword }  from 'firebase/auth'
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYPkx_zOZmfNvQloA9ZkjZh80xtI56_Ns",
+  authDomain: "mealstogo-7bc9a.firebaseapp.com",
+  projectId: "mealstogo-7bc9a",
+  storageBucket: "mealstogo-7bc9a.appspot.com",
+  messagingSenderId: "1007401999647",
+  appId: "1:1007401999647:web:3a45fe5a0f58724969242a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 export const LoginRequest = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
