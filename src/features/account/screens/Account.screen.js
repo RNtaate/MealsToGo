14a81d3ae24cdfe80522c6b4d6564d37f29fa4ee
RNtaate <royhyde14@gmail.com>
2022-11-30 12:44:19 +0300

    Add AccountCover, AccountButtonHolder and AccountButtons to the AccountScreen

--- src/features/account/screens/Account.screen.js
+++ src/features/account/screens/Account.screen.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button } from 'react-native-paper';
 
-import { AccountBackground } from '../components/Account.styles';
+import { AccountBackground, AccountCover, AccountButtonHolder, AccountButton } from '../components/Account.styles';
+import { Spacer } from '../../../components/Spacer/Spacer.component';
 
 const AccountScreen = () => {
   return (
-    <AccountBackground />
+    <AccountBackground>
+      <AccountCover />
+      <AccountButtonHolder>
+        <AccountButton icon="account-lock-open-outline" mode="contained" onPress={() => null} >
+          Login
+        </AccountButton>
+
+        <Spacer position="top" size="large" >
+          <AccountButton icon="account-plus-outline" mode="contained" onPress={() => null} >
+            Sign Up
+          </AccountButton>
+        </Spacer>
+      </AccountButtonHolder>
+    </AccountBackground>
   )
 }
 
