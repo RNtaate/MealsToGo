e1c37a38770edbe0489de1f27995412b1ff054cc
RNtaate <royhyde14@gmail.com>
2022-11-30 14:11:01 +0300

    Update the login screen using the AuthInput styled component
    Provide input for email and password for loging in

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
-import { AccountBackground } from '../components/Account.styles';
-import { Text } from '../../../components/typography/text.component';
+import { AccountBackground, AccountButton, AccountContainer, AuthInput } from '../components/Account.styles';
+import { Spacer } from '../../../components/Spacer/Spacer.component';
 
 const LoginScreen = () => {
   return (
     <AccountBackground>
-      <Text variant="body" >Login Screen</Text>
+      <AccountContainer>
+        <AuthInput
+          label="Email"
+          onChangeText={() => null }
+          mode='flat'
+          autoCapitalize="none"
+          autoComplete="off"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+
+        <Spacer position="vertical" size="large">
+          <AuthInput
+            label="Password"
+            autoCapitalize={'none'}
+            secureTextEntry={true}
+          />
+        </Spacer>
+
+        <AccountButton icon="lock-open-outline" mode="contained" onPress={() => null} >
+          Login
+        </AccountButton>
+      </AccountContainer>
     </AccountBackground>
   )
 }
