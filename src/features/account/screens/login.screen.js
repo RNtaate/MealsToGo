a9abe94afe63ac802d66481423b68fb399ec1aca
RNtaate <royhyde14@gmail.com>
2022-11-30 14:49:54 +0300

    Implement the logging in functionality using authentication context.

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import { AccountBackground, AccountButton, AccountContainer, AuthInput } from '../components/Account.styles';
 import { Spacer } from '../../../components/Spacer/Spacer.component';
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
 
 const LoginScreen = () => {
+
+  const { onLogin, isLoading } = useContext(AuthenticationContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (email, password) => {
+    if(!email.length || !password.length ) {
+      console.log("Stuff cannot be empty");
+      return
+    }
+
+    onLogin(email, password);
+  }
+
   return (
     <AccountBackground>
       <AccountContainer>
         <AuthInput
           label="Email"
-          onChangeText={() => null }
+          onChangeText={(text) => setEmail(text) }
           mode='flat'
           autoCapitalize="none"
           autoComplete="off"
           keyboardType="email-address"
           textContentType="emailAddress"
+          value={email}
         />
 
         <Spacer position="vertical" size="large">
@@ -22,10 +38,18 @@ const LoginScreen = () => {
             label="Password"
             autoCapitalize={'none'}
             secureTextEntry={true}
+            value={password}
+            onChangeText={(text) => setPassword(text) }
           />
         </Spacer>
 
-        <AccountButton icon="lock-open-outline" mode="contained" onPress={() => null} >
+        <AccountButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => handleLogin(email, password)}
+          loading={isLoading}
+          disabled={isLoading}
+        >
           Login
         </AccountButton>
       </AccountContainer>
