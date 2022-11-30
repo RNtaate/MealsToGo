c2089c94bc1d4fc2a9008c1cf487f523a15a76ef
RNtaate <royhyde14@gmail.com>
2022-11-30 12:43:37 +0300

    Create AccountCover, AccountButtonHolder styles
    Create AccountButton using Button from react-native-paper

--- src/features/account/components/Account.styles.js
+++ src/features/account/components/Account.styles.js
@@ -1,5 +1,6 @@
-import { ImageBackground } from 'react-native';
+import { ImageBackground, View } from 'react-native';
 import styled from 'styled-components/native';
+import { Button } from 'react-native-paper';
 
 export const AccountBackground = styled(ImageBackground).attrs({
   source: require('../../../../assets/home_bg.jpg'),
@@ -7,4 +8,26 @@ export const AccountBackground = styled(ImageBackground).attrs({
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+export const AccountCover = styled(View)`
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.3);
+  width: 100%;
+  height: 100%;
+`;
+
+export const AccountButtonHolder = styled(View)`
+  max-width: 300px;
+  padding: ${({theme}) => theme.space[3]}
+  background-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const AccountButton = styled(Button).attrs((props) => ({
+  labelStyle: {
+    fontFamily: props.theme.fonts.body
+  }
+}))`
+  background-color: ${({theme}) => theme.colors.brand.primary};
+  padding: ${({theme}) => theme.space[1]}
 `;
